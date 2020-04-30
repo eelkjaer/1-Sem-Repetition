e2c25c21254b88f1058094510fd28d92d8790564
Emil Elkjær Nielsen <emil@visiontrade.dk>
2020-04-30 10:31:58 +0200

    Menu added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,56 +13,103 @@ import Sortering.*;
 import StortBogstav.*;
 import Strenge.*;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        //ArrayListe opgaven
-        StringToArrayList sta = new StringToArrayList();
-        //sta.runArrayList();
+    private static Scanner input = new Scanner(System.in);
 
-        //Bog opgaven
-        BogController bog = new BogController();
-        //bog.addSampleBooks();
+    public static void main(String[] args) {
+            try {
+                menu();
+            } catch(Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
 
-        //Hvor tit skal græsset slås
-        TidTilKlipning ttk = new TidTilKlipning();
-        //ttk.beregnMaksDage();
-
-        //Kvadrat
-        Kvadrat kv = new Kvadrat();
-        //kv.tegnKvadrat(6, "#");
-
-        //Menuvalg
-        Menu menu = new Menu();
-        //menu.vaelgMenu();
-
-        //Proper case
-        ProperCase prc = new ProperCase();
-        //System.out.println(prc.ensureCasing("HEJ med DiG SMuKKE fiskemand"));
-
-        //Regning
-        Regning regning = new Regning();
-        //regning.udregnResultat(40,30);
-        //regning.udregnResultat(1,50);
-
-        //Sortering
-        Sortering sort = new Sortering();
-        //sort.sorterInput();
-
-        //Stort bogstav
-        StortBogstav stb = new StortBogstav();
-        //stb.inputWords();
-
-        //Strenge
-        Strenge str = new Strenge();
-        //System.out.println(str.nyStreng("hej","med",'m','n'));
-
-        //Søgning
-        Sogning sog = new Sogning();
-        String[] array = new String[]{"hej","abe","mand"};
-        //System.out.println("Index: " + sog.findStrengIArray(array,"abe"));
-
+        public static void menu() throws Exception {
+        printMenu();
+            switch(Integer.parseInt(input.nextLine())) {
+                case 1:
+                    //ArrayListe opgaven
+                    StringToArrayList sta = new StringToArrayList();
+                    sta.runArrayList();
+                    menu();
+                case 2:
+                    //Bog opgaven
+                    BogController bog = new BogController();
+                    bog.addSampleBooks();
+                    menu();
+                case 3:
+                    //Hvor tit skal græsset slås
+                    TidTilKlipning ttk = new TidTilKlipning();
+                    ttk.beregnMaksDage();
+                    menu();
+                case 4:
+                    //Kvadrat
+                    Kvadrat kv = new Kvadrat();
+                    kv.tegnKvadrat(6, "#");
+                    menu();
+                case 5:
+                    //Menuvalg
+                    Menu menu = new Menu();
+                    //menu.vaelgMenu();
+                    menu();
+                case 6:
+                    //Proper case
+                    ProperCase prc = new ProperCase();
+                    System.out.println(prc.ensureCasing("HEJ med DiG SMuKKE fiskemand"));
+                    menu();
+                case 7:
+                    //Regning
+                    Regning regning = new Regning();
+                    regning.udregnResultat(40, 30);
+                    regning.udregnResultat(1, 50);
+                    menu();
+                case 8:
+                    //Sortering
+                    Sortering sort = new Sortering();
+                    sort.sorterInput();
+                    menu();
+                case 9:
+                    //Stort bogstav
+                    StortBogstav stb = new StortBogstav();
+                    stb.inputWords();
+                    menu();
+                case 10:
+                    //Strenge
+                    Strenge str = new Strenge();
+                    System.out.println(str.nyStreng("hej", "med", 'm', 'n'));
+                    menu();
+                case 11:
+                    //Søgning
+                    Sogning sog = new Sogning();
+                    String[] array = new String[]{"hej", "abe", "mand"};
+                    System.out.println("Index: " + sog.findStrengIArray(array, "abe"));
+                    menu();
+                case 12:
+                    System.out.println("Farvel!");
+                    System.exit(0);
+                default:
+                    menu();
+            }
     }
 
+    public static void printMenu(){
+        System.out.println("\n## Vælg en af følgende repetitionsopgaver ##");
+        System.out.println("1.\tArrayList");
+        System.out.println("2.\tBog");
+        System.out.println("3.\tHvor tit skal græsset slåes");
+        System.out.println("4.\tKvadrat");
+        System.out.println("5.\tMenuvalg");
+        System.out.println("6.\tProper case");
+        System.out.println("7.\tRegning");
+        System.out.println("8.\tSortering");
+        System.out.println("9.\tStort bogstav");
+        System.out.println("10.\tStrenge");
+        System.out.println("11.\tSøgning");
+        System.out.println("######");
+        System.out.println("12.\tAfslut program");
+    }
 
 }
